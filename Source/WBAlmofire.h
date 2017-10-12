1652d053c128f68d0f4792fbfa3732cedbce25b4
JsonBin <1120508748@qq.com>
2017-10-12 16:25:43 +0800

    add header and info

---
+++ Source/WBAlmofire.h
@@ -0,0 +1,17 @@
+//
+//  WBAlmofire.h
+//  WBAlamofire
+//
+//  Created by zwb on 2017/10/12.
+//  Copyright © 2017年 HengSu Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Alamofire-SwiftyJSON.
+FOUNDATION_EXPORT double WBAlmofire;
+
+//! Project version string for Alamofire-SwiftyJSON.
+FOUNDATION_EXPORT const unsigned char WBAlmofire[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WBAlmofire/WBAlmofire.h>
