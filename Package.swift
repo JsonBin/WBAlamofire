fcde66ac4c4d96d5ce1fdfa1cf9cc71a334972d6
zeng.wenbin <zeng.wenbin@uneed.com>
2019-05-16 17:53:46 +0800

    update package

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:4.0
+// swift-tools-version: 5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "WBAlamofire",
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", Version(4, 5, 0)..<Version(5, 0, 0))
+        .package(url: "https://github.com/Alamofire/Alamofire.git", Version(5, 0, 0)..<Version(6, 0, 0))
     ],
-    swiftLanguageVersions: [4]
+    swiftLanguageVersions: [5]
 )
